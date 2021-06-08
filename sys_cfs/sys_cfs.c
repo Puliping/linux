6c7eadab252ffd2ae7760dea50e95f349b0f81a0
Luiz Zanini <luiz.kasputis@hotmail.com>
2021-06-08 18:00:41 -0300

    git add syscfs

---
+++ sys_cfs/sys_cfs.c
@@ -0,0 +1,84 @@
+
+#include <linux/linkage.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/list.h>
+#include <linux/sched.h>
+#include <linux/sched/signal.h>  
+#include <linux/sched/mm.h>
+
+struct Node {
+    int pid;
+	unsigned long long virtual_time;
+    struct Node* next;
+};
+
+void sortedInsert(struct Node** head_ref, struct Node* new_node) {
+
+    struct Node* atual;
+
+    if (*head_ref == NULL || (*head_ref)->virtual_time  >= new_node->virtual_time) {
+
+        new_node->next = *head_ref;
+        *head_ref = new_node;
+    
+	}
+    else {
+
+        atual = *head_ref;
+
+        while (atual->next != NULL && atual->next->virtual_time < new_node->virtual_time) {
+        
+		    atual = atual->next;
+        
+		}
+
+        new_node->next = atual->next;
+        atual->next = new_node;
+
+    }
+}
+
+struct Node* newNode(int new_pid, unsigned long long new_vruntime) {
+
+    struct Node* new_node = (struct Node*)kmalloc( sizeof(struct Node), GFP_KERNEL );
+ 
+    /* put in the data  */
+    new_node->virtual_time = new_vruntime;
+	new_node->pid = new_pid;
+    new_node->next = NULL;
+
+    return new_node;
+}
+ 
+void printList(struct Node* head) {
+
+    struct Node* temp = head;
+	int cont = 0;
+    while (temp != NULL) {
+        pr_info("pid%d  virtualRuntime %llu \n", temp->pid, temp->virtual_time);
+		
+		temp = temp->next;
+    }
+}
+
+
+asmlinkage unsigned long sys_cfs(void){
+
+	struct task_struct* task_list;
+	struct Node* head = NULL;
+
+
+	for_each_process(task_list) {
+		
+		struct Node* new_node = newNode(task_list->pid, task_list->se.vruntime);
+		
+		sortedInsert(&head, new_node);
+
+	}
+
+	printList(head);
+
+	return 1 ;
+}
